5a8d585be7fb0ac60a17fdc155e17c2290ab05fc
CristianOvalle <cristianovallegutierrez@gmail.com>
2022-10-12 14:29:04 -0400

    Entity Framework

---
+++ ApiPeliculasEFCore/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using ApiPeliculasEFCore.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPeliculasEFCore
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Genero> Generos { get; set; }
+    }
+}
---
+++ ApiPeliculasEFCore/Entidades/Genero.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiPeliculasEFCore.Entidades
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+
+        public string nombre { get; set; }
+    }
+}
