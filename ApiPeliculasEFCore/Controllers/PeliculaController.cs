940ceb1b8ea680cab671ab94498b89000caa7985
CristianOvalle <cristianovallegutierrez@gmail.com>
2022-10-19 11:26:08 -0400

    EF Core

--- ApiPeliculasEFCore/Controllers/PeliculaController.cs
+++ ApiPeliculasEFCore/Controllers/PeliculaController.cs
@@ -41,5 +41,46 @@ namespace ApiPeliculasEFCore.Controllers
 
             return peliculaDTO;
         }
+
+        [HttpGet("cargaselectiva/{id:int}")]
+        public async Task<ActionResult> GetSelectivo(int id)
+        {
+            var pelicula = await context.Peliculas.Select(x =>
+            new
+            {
+                Id = x.Id,
+                Titulo = x.Titulo,
+                Generos = x.Genero.OrderByDescending(x => x.Nombre).Select(c => c.Nombre).ToList(),
+                cantidadActores = x.PeliculaActores.Count(),
+                cantidadCines = x.SalaDeCine.Select(s => s.CineId).Distinct().Count()
+            }).FirstOrDefaultAsync(p => p.Id == id);
+
+            if (pelicula is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pelicula);
+        }
+
+        [HttpGet("cargadoexplicito/{id:int}")]
+        public async Task<ActionResult<PeliculaDTOs>> GetExplicito(int id)
+        {
+            var pelicula = await context.Peliculas.AsTracking().FirstOrDefaultAsync(x => x.Id == id);
+            //...
+
+            //await context.Entry(pelicula).Collection(x => x.Genero).LoadAsync();
+
+            var cantidadGeneros = await context.Entry(pelicula).Collection(x => x.Genero).Query().CountAsync();
+
+            if (pelicula is null)
+            {
+                return NotFound();
+            }
+
+           var peliculaDTOs = mapper.Map<PeliculaDTOs>(pelicula);
+
+            return peliculaDTOs;
+        }
     }
 }
